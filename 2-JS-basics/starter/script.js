5f97d9e195590305936b8312186a9b0327fd9fca
ade bakari <redariez@gmail.com>
2019-06-27 17:43:08 -0700

    coding challenge 1

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -24,6 +24,7 @@ var johnMark = 'John  and Mark';
 Variable mutation and type coercion
 */
 
+/**
 var firstName = 'John';
 var age = 28;
 
@@ -43,11 +44,12 @@ job = 'driver';
 alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
 
 var lastName = prompt('What is his last name');
-console.log(firstName + ' ' + lastName);
+console.log(firstName + ' ' + lastName); */
 
 /**
 *Basic operaters
 */
+/**
 var year, yearJohn, yearMark;
 now = 2018;
 ageJohn = 28;
@@ -75,15 +77,58 @@ console.log(typeof johnOlder);
 console.log(typeof ageJohn);
 console.log(typeof 'Mark is older than John');
 var x;
-console.log(typeof x);
+console.log(typeof x); */
 
 /**************
 * Operator precedence
 */
 
 var now = 2018;
-var yearJohn = 1998;
+var yearJohn = 1989;
 var fullAge = 18;
 
+///Multiple operators
 var isFullAge = now - yearJohn >= fullAge;
 console.log(isFullAge);
+
+
+//Grouping
+var ageJohn = now - yearJohn;
+var ageMark = 35;
+var average = (ageJohn + ageMark)  / 2;
+console.log(average)
+
+//Multiple assignments
+var x, y;
+x = y = (3 + 5) * 4 - 6; // 8 * 4 - 6 // 32 - 6 // 26
+console.log(x, y);
+
+// 2 + 4 + 5
+
+// More operaters
+x *= 2;
+console.log(x);
+x += 10;
+console.log(x);
+x = x + 1;
+x += 1;
+x++
+console.log(x);
+
+/******************
+* CODING CHALLENGE
+*/
+
+var johnMass = 100;
+var johnHeight = 1.60;
+var markMass = 80;
+var markHeight = 1.20;
+
+bmiMark = markMass/(markHeight * markHeight);
+bmiJohn = johnMass/(johnHeight * johnHeight);
+
+console.log(bmiMark);
+console.log(bmiJohn);
+
+var isBmiMark = bmiMark > bmiJohn;
+console.log(" Is Marks BMI bigger than Johns?" + isBmiMark);
