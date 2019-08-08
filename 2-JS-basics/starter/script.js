02fa79ed08477e16ee9e00f788bb0f67242e6657
ade bakari <redariez@gmail.com>
2019-08-08 14:42:29 -0700

    objects

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -352,11 +352,46 @@ names[names.length] = 'Mary';
 console.log(names);
 
 //Different data types
-var john = ['John', 'Smith', 1990, 'teacher', false];
+var john = ['John', 'Smith', 1990, 'teacher', 'designer', false];
 
 john.push('blue');
 john.unshift('Mr.');
 console.log(john);
 
 john.pop();
+john.pop();
+john.shift();
 console.log(john);
+
+console.log(john.indexOf('teacher'));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is Not a designer' : 'John IS a designer';
+console.log(isDesigner)
+
+/**************
+* Objects and properties
+*/
+//Object literal
+var john = {
+  firstName: 'John',
+  lastName: 'Smith',
+  birthYear: 1990,
+  family: ['Jane', 'Mark', 'Bob', 'Emily'],
+  job: 'Coder',
+  isMarried: false
+};
+console.log(john.firstName);
+console.log(john['lastName']);
+var x = 'birthYear';
+console.log(john[x]);
+
+john.job = 'designer';
+john['isMarried'] = true;
+console.log(john);
+
+//new Object syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = 1969;
+jane['lastName'] = 'Smith';
+console.log(jane);
